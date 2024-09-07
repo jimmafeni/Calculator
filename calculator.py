0eebcef17f11989d9d1077d9d9a1b5d09699cdc3
jimmase <jimiama.mafenimase2@nottingham.ac.uk>
2024-09-07 13:06:17 +0100

    Created a functionality to add two numbers

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def add(num1, num2):
+    return num1 + num2
+   
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Add the two numbers
+result = add(num1, num2)
+
+# Display the result
+print(f"The sum of {num1} and {num2} is: {result}")
